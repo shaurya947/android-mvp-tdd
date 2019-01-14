238df3571f01cf43b135bc58e3094b5ede91ed55
Shaurya Arora <sarora@tribalscale.com>
2019-01-14 16:10:59 -0500

    Move all logic inside Activity, update tests.

--- app/src/main/java/com/example/tsl018/tdddemo/activities/UserInformationActivity.kt
+++ app/src/main/java/com/example/tsl018/tdddemo/activities/UserInformationActivity.kt
@@ -5,34 +5,46 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.tsl018.tdddemo.DemoApp
 import com.example.tsl018.tdddemo.R
-import com.example.tsl018.tdddemo.contract.UserInformationContract
+import com.example.tsl018.tdddemo.database.DemoDatabase
+import com.example.tsl018.tdddemo.models.User
 import com.example.tsl018.tdddemo.network.NetworkClient
-import com.example.tsl018.tdddemo.presenter.UserInformationPresenter
+import com.example.tsl018.tdddemo.network.NetworkClientInterface
 import kotlinx.android.synthetic.main.activity_user_information.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import org.jetbrains.annotations.TestOnly
 import kotlin.coroutines.CoroutineContext
 
-class UserInformationActivity : AppCompatActivity(), UserInformationContract.View, CoroutineScope {
-    lateinit var job: Job
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
+class UserInformationActivity : AppCompatActivity(), CoroutineScope {
+    override lateinit var coroutineContext: CoroutineContext
+        @TestOnly set
 
-    private lateinit var presenter: UserInformationContract.Presenter
+    lateinit var user: User
+        @TestOnly set
+    lateinit var IOContext: CoroutineContext
+        @TestOnly set
+    lateinit var database: DemoDatabase
+        @TestOnly set
+    lateinit var networkClient: NetworkClientInterface
+        @TestOnly set
+
+    private lateinit var job: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        job = Job()
         setContentView(R.layout.activity_user_information)
-        presenter = UserInformationPresenter(this, coroutineContext, Dispatchers.IO, NetworkClient, DemoApp.DB)
-        btn_increment_age.setOnClickListener { presenter.onIncrementButtonClicked() }
+
+        job = Job()
+        coroutineContext = Dispatchers.Main +  job
+        IOContext = Dispatchers.IO
+        database = DemoApp.DB
+        networkClient = NetworkClient
+
+        btn_increment_age.setOnClickListener { onIncrementButtonClicked() }
     }
 
     override fun onStart() {
         super.onStart()
-        presenter.loadUserInfo()
+        loadUserInfo()
     }
 
     override fun onDestroy() {
@@ -40,21 +52,47 @@ class UserInformationActivity : AppCompatActivity(), UserInformationContract.Vie
         job.cancel()
     }
 
-    override fun showUserInfo(s: String) {
+    private fun showUserInfo(s: String) {
         loading_view.visibility = View.GONE
         info_view.visibility = View.VISIBLE
         info_view.text = s
         btn_increment_age.visibility = View.VISIBLE
     }
 
-    override fun showError() {
+    private fun showError() {
         loading_view.visibility = View.GONE
         info_view.visibility = View.VISIBLE
         info_view.text = "ERROR!"
     }
 
-    @TestOnly
-    fun setTestPresenter(testPresenter: UserInformationContract.Presenter) {
-        presenter = testPresenter
+    private fun loadUserInfo() {
+        launch {
+            try {
+                val users = withContext(IOContext) { database.userDao().getAllUsers() }
+                val user = users.firstOrNull()
+                user?.let { updateUserAndShowInfo(it) } ?: run {
+                    val newUser = networkClient.getUser(IOContext).await()
+                    newUser?.let {
+                        withContext(IOContext) { database.userDao().insert(it) }
+                        updateUserAndShowInfo(it)
+                    }
+                }
+            } catch (e: Exception) {
+                showError()
+            }
+        }
+    }
+
+    private fun updateUserAndShowInfo(userToShow: User) {
+        user = userToShow
+        showUserInfo("${user.firstName} ${user.lastName}, ${user.age}")
+    }
+
+    private fun onIncrementButtonClicked() {
+        launch {
+            val updatedUser = User(user.firstName, user.lastName, user.age.plus(1))
+            withContext(IOContext) { database.userDao().updateUser(updatedUser) }
+            loadUserInfo()
+        }
     }
 }
--- app/src/test/java/com/example/tsl018/tdddemo/activities/UserInformationActivityTest.kt
+++ app/src/test/java/com/example/tsl018/tdddemo/activities/UserInformationActivityTest.kt
@@ -1,15 +1,21 @@
 package com.example.tsl018.tdddemo.activities
 
 import android.view.View
-import com.example.tsl018.tdddemo.contract.UserInformationContract
+import com.example.tsl018.tdddemo.database.DemoDatabase
+import com.example.tsl018.tdddemo.database.UserDao
+import com.example.tsl018.tdddemo.models.User
+import com.example.tsl018.tdddemo.network.NetworkClientInterface
 import kotlinx.android.synthetic.main.activity_user_information.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
 import org.robolectric.Robolectric
@@ -21,8 +27,20 @@ class UserInformationActivityTest {
     private lateinit var activityController: ActivityController<UserInformationActivity>
     private lateinit var activity: UserInformationActivity
 
+    private val user = User("Jamie", "Postones", 42)
+
+
     @Mock
-    private lateinit var presenter: UserInformationContract.Presenter
+    private lateinit var database: DemoDatabase
+
+    @Mock
+    private lateinit var networkClient: NetworkClientInterface
+
+    @Mock
+    private lateinit var userDeferred: Deferred<User?>
+
+    @Mock
+    private lateinit var userDao: UserDao
 
     @Rule
     @JvmField
@@ -30,10 +48,17 @@ class UserInformationActivityTest {
 
     @Before
     fun setUp() {
+        `when`(database.userDao()).thenReturn(userDao)
+        `when`(networkClient.getUser(Dispatchers.Unconfined)).thenReturn(userDeferred)
+
         activityController = Robolectric.buildActivity(UserInformationActivity::class.java)
         activity = activityController.get()
 
         activityController.create()
+        activity.coroutineContext = Dispatchers.Unconfined
+        activity.IOContext = Dispatchers.Unconfined
+        activity.database = database
+        activity.networkClient = networkClient
     }
 
     @Test
@@ -50,15 +75,14 @@ class UserInformationActivityTest {
     }
 
     @Test
-    fun invokesPresenterOnStart() {
-        activity.setTestPresenter(presenter)
+    fun `displays user information from network when not available locally`() = runBlocking {
+        `when`(userDao.getAllUsers()).thenReturn(listOf())
+        `when`(userDeferred.await()).thenReturn(user)
         activityController.start()
-        verify(presenter).loadUserInfo()
-    }
+        verify(userDao).getAllUsers()
+        verify(userDeferred).await()
+        verify(userDao).insert(user)
 
-    @Test
-    fun displaysUserInfoWhenInvoked() {
-        activity.showUserInfo("Jamie Postones, 42")
         assertEquals(View.GONE, activity.loading_view.visibility)
         assertEquals(View.VISIBLE, activity.info_view.visibility)
         assertEquals(View.VISIBLE, activity.btn_increment_age.visibility)
@@ -66,8 +90,24 @@ class UserInformationActivityTest {
     }
 
     @Test
-    fun displaysErrorWhenInvoked() {
-        activity.showError()
+    fun `displays user information from DB if available`() = runBlocking {
+        `when`(userDao.getAllUsers()).thenReturn(listOf(user))
+        activityController.start()
+        verify(userDao).getAllUsers()
+        verifyZeroInteractions(networkClient)
+
+        assertEquals(View.GONE, activity.loading_view.visibility)
+        assertEquals(View.VISIBLE, activity.info_view.visibility)
+        assertEquals(View.VISIBLE, activity.btn_increment_age.visibility)
+        assertEquals("Jamie Postones, 42", activity.info_view.text)
+    }
+
+    @Test
+    fun `displays error in case of DB exception`() = runBlocking {
+        `when`(userDao.getAllUsers()).thenAnswer { throw Exception() }
+        activityController.start()
+        verifyZeroInteractions(networkClient)
+
         assertEquals(View.GONE, activity.loading_view.visibility)
         assertEquals(View.VISIBLE, activity.info_view.visibility)
         assertEquals(View.GONE, activity.btn_increment_age.visibility)
@@ -75,9 +115,31 @@ class UserInformationActivityTest {
     }
 
     @Test
-    fun `clicking increment age button notifies presenter`() {
-        activity.setTestPresenter(presenter)
+    fun `displays error in case of network exception`() = runBlocking {
+        `when`(userDao.getAllUsers()).thenReturn(listOf())
+        `when`(userDeferred.await()).thenAnswer { throw Exception() }
+        activityController.start()
+        verify(userDao).getAllUsers()
+
+        assertEquals(View.GONE, activity.loading_view.visibility)
+        assertEquals(View.VISIBLE, activity.info_view.visibility)
+        assertEquals(View.GONE, activity.btn_increment_age.visibility)
+        assertEquals("ERROR!", activity.info_view.text)
+    }
+
+    @Test
+    fun `clicking increment age button updates user`() {
+        `when`(userDao.getAllUsers()).thenReturn(listOf(user))
+        activityController.start()
+
+        val updatedUser = User(user.firstName, user.lastName, user.age.plus(1))
+        `when`(userDao.getAllUsers()).thenReturn(listOf(updatedUser))
         activity.btn_increment_age.performClick()
-        verify(presenter).onIncrementButtonClicked()
+
+        verify(userDao).updateUser(updatedUser)
+        assertEquals(View.GONE, activity.loading_view.visibility)
+        assertEquals(View.VISIBLE, activity.info_view.visibility)
+        assertEquals(View.VISIBLE, activity.btn_increment_age.visibility)
+        assertEquals("Jamie Postones, 43", activity.info_view.text)
     }
 }
